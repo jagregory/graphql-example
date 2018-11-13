5c0be3e2292138c3fd02c21f9f32fca96f165d2c
James Gregory <james@jagregory.com>
2018-11-13 13:56:58 +1100

    GraphQL schema

--- server/index.js
+++ server/index.js
@@ -1,7 +1,40 @@
 import * as database from "./database";
 import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const typeDefs = gql`
+  type Query {
+    users(active: Boolean = true): [User]
+  }
+
+  type User {
+    id: String
+    firstName: String
+    lastName: String
+    email: String
+    billCode: String
+    visits: Int
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: (root, args) =>
+      database.users().filter(user => user.active === args.active)
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  engine: {
+    apiKey: "service:jagregory-8199:4uWEFHydc2Myo_fd2ZZXyQ"
+  }
+});
 
 const app = express();
+
+server.applyMiddleware({ app });
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello, world!");
